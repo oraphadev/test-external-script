6070729451a252ef32dad2018824b0e2c2a1a407
Raphael Lima <oraphadev@gmail.com>
2025-01-22 21:48:11 -0300

    chore(master): test iframe widget

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 (function () {
-  console.log('Hello, World!');
-  console.log(window.location.href);
+  const iframe = document.createElement("iframe");
+  iframe.src = "https://f829-187-85-18-74.ngrok-free.app/widget";
+  iframe.style.position = "fixed";
+  iframe.style.bottom = "20px";
+  iframe.style.right = "20px";
+  iframe.style.width = "120px";
+  iframe.style.height = "120px";
+  iframe.style.border = "none";
+  iframe.style.zIndex = "9999";
+  document.body.appendChild(iframe);
 })();
